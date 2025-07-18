7bf2f76944ec677175d4362eb4ceaf067c86931c
Bashar Ahmad Khan <150275763+Basharkhan243@users.noreply.github.com>
2025-07-19 04:56:33 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -4,7 +4,7 @@ import cookieParser from 'cookie-parser';
 const app= express();
 
 app.use(cors({
-    origin: process.env.CORS_ORIGIN || "http://localhost:5173",
+    origin: process.env.CORS_ORIGIN || "https://scribly-frontend-j8ps.vercel.app/",
     credentials: true 
 }))
 
